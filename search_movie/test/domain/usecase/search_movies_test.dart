7715fb4862109f124e8ea1b6cbd176d7cde2d241
rizkyalfikri00 <rizkyalfikri@gmail.com>
2022-09-18 19:09:29 +0700

    fix failing unit test

--- search_movie/test/domain/usecase/search_movies_test.dart
+++ search_movie/test/domain/usecase/search_movies_test.dart
@@ -1,11 +1,16 @@
 import 'package:core/domain/entities/movie.dart';
+import 'package:core/domain/repositories/movie_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:search_movie/search_movie.dart';
+import 'package:search_movie/domain/usecase/search_movies.dart';
 
-import '../../../../core/test/helpers/test_helper.mocks.dart';
+import 'search_movies_test.mocks.dart';
 
+@GenerateMocks([
+  MovieRepository,
+])
 void main() {
   late SearchMovies usecase;
   late MockMovieRepository mockMovieRepository;
@@ -16,7 +21,7 @@ void main() {
   });
 
   final tMovies = <Movie>[];
-  const tQuery = 'Spiderman';
+  const tQuery = 'Naruto';
 
   test('should get list of movies from the repository', () async {
     // arrange
